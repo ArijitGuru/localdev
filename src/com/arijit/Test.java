bda00fce0b9b28d42c14fbeff2af2d33b2ac006a
Dell_Lattitude <Dell_Lattitude@192.168.0.12>
2018-04-07 23:23:31 -0400

    Arijit - commit 3

--- src/com/arijit/Test.java
+++ src/com/arijit/Test.java
@@ -6,7 +6,6 @@ import java.io.InputStreamReader;
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		int a = 10;
 		int b = 20;
 		int c = a+b;
