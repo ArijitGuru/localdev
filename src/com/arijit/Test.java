52598da72e8730f5a8869d7c8dbb30944a453eb6
Dell_Lattitude <Dell_Lattitude@DESKTOP-IIV7CRN>
2018-05-24 22:19:57 -0400

    updates

--- src/com/arijit/Test.java
+++ src/com/arijit/Test.java
@@ -1,7 +1,4 @@
 package com.arijit;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 
 public class Test {
 
@@ -9,16 +6,10 @@ public class Test {
 		int a = 10;
 		int b = 20;
 		int c = a+b;
-		System.out.println("Enter number ...: ");
-		BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
-		try {
-				int value = Integer.parseInt(br.readLine());
-				System.out.println("Input is ...: " + value);
-				System.out.println("Sum is ...: " + c);
-			
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		//System.out.println("Enter number ...: ");
+		//int value = Integer.parseInt(br.readLine());
+		//System.out.println("Input is ...: " + value);
+		System.out.println("Sum is ...: " + c);
 		
 	}
 
