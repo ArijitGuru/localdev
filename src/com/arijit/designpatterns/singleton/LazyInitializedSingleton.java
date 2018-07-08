b668c4f9735980e549dd8e7cc1264971bf4b9d77
Dell_Lattitude <Dell_Lattitude@192.168.0.17>
2018-07-08 09:17:22 -0400

    singleton#3

---
+++ src/com/arijit/designpatterns/singleton/LazyInitializedSingleton.java
@@ -0,0 +1,20 @@
+package com.arijit.designpatterns.singleton;
+
+public class LazyInitializedSingleton {
+	private LazyInitializedSingleton instance = null;
+	
+	public LazyInitializedSingleton getInstance () {
+		if (instance == null) {
+			instance = new LazyInitializedSingleton();
+		}
+		return instance;
+	}
+	
+	/*
+	 * The above implementation works fine incase of single threaded environment but when it comes to multithreaded systems, 
+	 * it can cause issues if multiple threads are inside the if loop at the same time. 
+	 * It will destroy the singleton pattern and both threads will get the different instances of singleton class. 
+	 * In next section, we will see different ways to create a thread-safe singleton class.
+	 */
+
+}
