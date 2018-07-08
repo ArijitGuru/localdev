81264b543fae27de09d3adb605f0a924f023268b
Dell_Lattitude <Dell_Lattitude@192.168.0.17>
2018-07-08 09:04:41 -0400

    singleton#2

---
+++ src/com/arijit/designpatterns/singleton/StaticBlockInitializedSingleton.java
@@ -0,0 +1,24 @@
+package com.arijit.designpatterns.singleton;
+
+/*
+ * Both eager initialization and static block initialization creates the instance even before it’s being used and 
+ * that is not the best practice to use. So in further sections, 
+ * we will learn how to create Singleton class that supports lazy initialization.
+ */
+
+public class StaticBlockInitializedSingleton {
+	private static StaticBlockInitializedSingleton instance = null;
+	
+	static {
+		try {
+			instance = new StaticBlockInitializedSingleton();
+		}catch (Exception e) {
+			System.out.println("Unable to create the StaticBlockInitializedSingleton instance" + e.getMessage());
+		}
+	}
+	
+	public static StaticBlockInitializedSingleton getInstance () {
+		return instance;
+	}
+
+}
